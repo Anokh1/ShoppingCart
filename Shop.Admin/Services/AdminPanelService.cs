2320ec710da1bafcbf7b873214b05557cb81712f
Adrian00I <aokhadrian@gmail.com>
2022-09-29 22:55:26 +0800

    Video 1, 2, and 3

---
+++ Shop.Admin/Services/AdminPanelService.cs
@@ -0,0 +1,44 @@
+﻿using Shop.DataModels.CustomModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Shop.Admin.Services
+{
+    public class AdminPanelService : IAdminPanelService
+    {
+        private readonly HttpClient httpClient;
+
+        public AdminPanelService(HttpClient _httpClient)
+        {
+            this.httpClient = _httpClient;
+        }
+        public async Task<ResponseModel> AdminLogin(LoginModel loginModel)
+        {
+            return await httpClient.PostJsonAsync<ResponseModel>("api/admin/AdminLogin", loginModel);
+        }
+
+        public async Task<CategoryModel> SaveCategory(CategoryModel newCategory)
+        {
+            return await httpClient.PostJsonAsync<CategoryModel>("api/admin/SaveCategory", newCategory);
+        }
+
+        public async Task<List<CategoryModel>> GetCategories()
+        {
+            return await httpClient.GetJsonAsync<List<CategoryModel>>("api/admin/GetCategories");
+        }
+
+        public async Task<bool> UpdateCategory(CategoryModel categoryToUpdate)
+        {
+            return await httpClient.PostJsonAsync<bool>("api/admin/UpdateCategory", categoryToUpdate);
+        }
+
+        public async Task<bool> DeleteCategory(CategoryModel categoryToDelete)
+        {
+            return await httpClient.PostJsonAsync<bool>("api/admin/DeleteCategory", categoryToDelete);
+        }
+    }
+}
